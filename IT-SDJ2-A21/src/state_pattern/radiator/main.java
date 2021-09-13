0aee767831be373cf13cc3d5d488a0e334b9cc57
Jordi Lazo <jordilazo94@gmail.com>
2021-09-13 13:27:50 +0200

    VIA

--- IT-SDJ2-A21/src/state_pattern/radiator/main.java
+++ IT-SDJ2-A21/src/state_pattern/radiator/main.java
@@ -4,7 +4,7 @@ public class main {
     public static void main(String[] args) throws InterruptedException {
         Radiator r = new Radiator();
         r.setPowerState(new Power3State(r));
-        System.out.println("Radiator level is: " + r.getPower());
+        System.out.println("Radiator level : " + r.getPower());
         System.out.println("Waiting...");
         Thread.sleep(11000);
         //r.turnDown(r);
