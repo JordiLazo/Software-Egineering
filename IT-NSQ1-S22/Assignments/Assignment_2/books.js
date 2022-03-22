557aab212ceada563b47e36a33092ba130784643
Jordi Lazo <jordilazo94@gmail.com>
2022-03-22 09:05:27 +0100

    VIA

---
+++ IT-NSQ1-S22/Assignments/Assignment_2/books.js
@@ -0,0 +1,50 @@
+books = [
+    {
+        "title": "harry potter1",
+        "author": "jk",
+        "soldCopies":"100",
+        "bookPrice":"100",
+        "ISBN": "1234",
+        "pages":"200",
+        "idCategory":"fantasy",
+        "idSubcategory":"magic",
+    },
+    {
+        "title": "harry potter2",
+        "author": "jk",
+        "soldCopies":"100",
+        "bookPrice":"100",
+        "ISBN": "1235",
+        "pages":"300",
+        "idCategory":"fantasy",
+        "idSubcategory":"magic",
+    },
+    {
+        "title": "harry potter3",
+        "author": "jk",
+        "soldCopies":"100",
+        "bookPrice":"100",
+        "ISBN": "1236",
+        "pages":"400",
+        "idCategory":"fantasy",
+        "idSubcategory":"magic",
+    }
+]
+
+customer = [
+    {
+        "idCustomer":"1",
+        "idOrder":"123456",
+        "address":"kamtjatka 11",
+        "totalSales":"2"
+    }
+]
+
+orders = [
+    {
+        "idOrder":"123456",
+        "ISBN":"1234",
+        "quantity":"2",
+        "date":"18/03/2022"
+    }
+]
